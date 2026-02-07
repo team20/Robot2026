b5f1be82eda07798df054e5181a4bc2284d8039f
thechessnoob <boeing777.9er@gmail.com>
2026-02-06 19:46:49 -0500

    Refactor IntakeSubsystem for SparkMax motor control
    Refactor IntakeSubsystem to use SparkMax motor and add methods for motor initialization and speed setting.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,12 +1,30 @@
 package frc.robot.subsystems;
 
 // import motor stuff
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+// import more stuff
+import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+import static frc.robot.Constants.DriveConstants.*;
 
 public class IntakeSubsystem
 {
+  private SparkMax m_motor;
+  
   public IntakeSubsystem()
   {
+    
   }
+  
+  public motor(int id)
+  {
+    m_motor = new SparkMax(id, MotorType.kBrushless);
+  }
+  public setSpeed(double speed)
+  {
+    m_motor.set(speed);
+  }
+  public setSpeed(0.1);
 }
