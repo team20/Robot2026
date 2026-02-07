98df42f39c753fa378b9f8f037813a64c4cfdae2
thechessnoob <boeing777.9er@gmail.com>
2026-02-06 20:05:47 -0500

    Add IntakeCommand class for intake subsystem control

---
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeCommand.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.IntakeCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class IntakeCommand
+{
+  public void initialize()
+  {
+  }
+
+  public void execute()
+  {
+  }
+
+  public boolean isFinished()
+  {
+  }
+  public void end()
+  {
+  }
+}
