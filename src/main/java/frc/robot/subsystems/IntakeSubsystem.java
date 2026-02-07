5d7bd34ceea6123f0ec58553d3a3f93a46b7bda9
thechessnoob <boeing777.9er@gmail.com>
2026-02-06 19:23:27 -0500

    Add motor imports to IntakeSubsystem
    Added imports for motor control in IntakeSubsystem.

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+// import motor stuff
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class IntakeSubsystem
 {
   public IntakeSubsystem()
