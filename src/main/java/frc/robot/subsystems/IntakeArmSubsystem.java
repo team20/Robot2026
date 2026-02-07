ec897404d68ef90de19c2432d4f2f0e6710206f2
thechessnoob <boeing777.9er@gmail.com>
2026-02-06 20:36:30 -0500

    Add SparkMax imports to IntakeArmSubsystem
    Added imports for SparkMax motor control

--- src/main/java/frc/robot/subsystems/IntakeArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeArmSubsystem.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems;
 
+// import motor stuff
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+// import other stuff
 import static edu.wpi.first.units.Units.*;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 import static frc.robot.Constants.IntakeConstants.*;
