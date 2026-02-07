a9d28e46d3deb089d8e2278d37348d22baf9350d
thechessnoob <boeing777.9er@gmail.com>
2026-02-07 14:37:00 -0400

    stuff
    stuff

--- src/main/java/frc/robot/commands/IntakeCommands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeCommand.java
@@ -1,28 +1,19 @@
 package frc.robot.commands.IntakeCommands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.IntakeSubsystem;
+public class IntakeCommand {
+	public void IntakeCommand() {
+	}
 
-public class IntakeCommand
-{
-  public void IntakeCommand()
-  {
-  }
+	public void initialize() {
+	}
 
-  public void initialize()
-  {
-  }
+	public void execute() {
+	}
 
-  public void execute()
-  {
-  }
+	public boolean isFinished() {
+		return true;
+	}
 
-  public boolean isFinished()
-  {
-    return true;
-  }
-  
-  public void end()
-  {
-  }
+	public void end() {
+	}
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,22 +3,22 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 // import motor stuff
 import com.revrobotics.spark.SparkMax;
-
-import frc.robot.Constants.IntakeConstants;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class IntakeSubsystem {
 	private SparkMax m_motor;
 
-	// initialize stuff
-	@SuppressWarnings("deprecation")
+	// config
+	SparkMaxConfig config = new SparkMaxConfig();
+	// set config values
+	config.inverted(IntakeConstants.kInvert);
+	config.smartCurrentLimit(IntakeConstants.kSmartCurrentLimit);
+
 	public IntakeSubsystem() {
 		// can id is a placeholder; replace later.
 		m_motor = new SparkMax(10, MotorType.kBrushless);
-		// class SparkBaseConfig smartcurrentlimit(int stalllimit)
-		// set current limits
-		m_motor.setSmartCurrentLimit(IntakeConstants.kSmartCurrentLimit);
-		m_motor.setInverted(IntakeConstants.kInvert);
-
+		// Apply config to SparkMax
+		m_motor.apply(config);
 	}
 
 	// set speed for motor
