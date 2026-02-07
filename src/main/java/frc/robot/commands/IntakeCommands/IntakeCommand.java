fee8a7c7a2a0b284ec51664ce0b8be86655d9602
thechessnoob <boeing777.9er@gmail.com>
2026-02-07 14:37:00 -0400

    stuff
    stuff

---
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakeCommand.java
@@ -0,0 +1,19 @@
+package frc.robot.commands.IntakeCommands;
+
+public class IntakeCommand {
+	public void IntakeCommand() {
+	}
+
+	public void initialize() {
+	}
+
+	public void execute() {
+	}
+
+	public boolean isFinished() {
+		return true;
+	}
+
+	public void end() {
+	}
+}
