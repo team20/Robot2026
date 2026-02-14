27bd01dae0ea4cb8a729d771bd6d5fea97c59c3b
Yunni-0 <sonicbabyyeah@gmail.com>
2026-02-14 10:27:45 -0500

    Rebase onto main

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+<<<<<<< HEAD
 import static frc.robot.Constants.IntakeConstants.*;
 
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -37,5 +38,31 @@ public class IntakeSubsystem extends SubsystemBase {
 	 */
 	public void setWheelPower(double power) {
 		m_intakeWheels.set(power);
+=======
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+// import motor stuff
+import com.revrobotics.spark.SparkMax;
+
+import frc.robot.Constants.IntakeConstants;
+
+public class IntakeSubsystem {
+	private SparkMax m_motor;
+
+	// initialize stuff
+	@SuppressWarnings("deprecation")
+	public IntakeSubsystem() {
+		// can id is a placeholder; replace later.
+		m_motor = new SparkMax(10, MotorType.kBrushless);
+		// class SparkBaseConfig smartcurrentlimit(int stalllimit)
+		// set current limits
+		m_motor.setSmartCurrentLimit(IntakeConstants.kSmartCurrentLimit);
+		m_motor.setInverted(IntakeConstants.kInvert);
+
+	}
+
+	// set speed for motor
+	public void setSpeed(double speed) {
+		m_motor.set(speed);
+>>>>>>> 4a47a4e (67)
 	}
 }
