45925d0b2853a37bcd505e0e8d7078e9c3e7adde
Yunni-0 <sonicbabyyeah@gmail.com>
2026-02-14 15:26:06 -0500

    Uncomment other subsystems

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,8 +9,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import frc.robot.commands.DriveCommands;
+import frc.robot.commands.HoodCommands;
 import frc.robot.commands.IntakeCommands;
+import frc.robot.commands.ShooterCommands;
+import frc.robot.commands.TurretCommands;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Hood;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Turret;
 
 public class Robot extends TimedRobot {
 	private CommandScheduler m_scheduler = CommandScheduler.getInstance();
@@ -21,10 +29,10 @@ public class Robot extends TimedRobot {
 			Constants.ControllerConstants.kOperatorControllerPort);
 
 	{
-		// new Drive();
-		// new Shooter();
-		// new Turret();
-		// new Hood();
+		new Drive();
+		new Shooter();
+		new Turret();
+		new Hood();
 		new Intake();
 	}
 
@@ -33,23 +41,21 @@ public class Robot extends TimedRobot {
 	}
 
 	private void bindControls() {
-		/*
-		 * Drive.getDrive().setDefaultCommand(
-		 * new DriveCommands.JoystickDrive(
-		 * () -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(),
-		 * () -> m_driverController.getL2Axis() - m_driverController.getR2Axis(),
-		 * m_driverController.getHID()::getCreateButton));
-		 * Turret.getTurret().setDefaultCommand(
-		 * new TurretCommands.RunToAngleHardwareSignal(m_operatorController::getLeftX,
-		 * m_operatorController::getLeftY));
-		 * m_operatorController.L1().whileTrue(new TurretCommands.RunAtPower(-.1, 0));
-		 * m_operatorController.R1().whileTrue(new TurretCommands.RunAtPower(.1, 0));
-		 * m_operatorController.triangle().toggleOnTrue(
-		 * new ShooterCommands.RunAtDPadRPM(this, m_operatorController.povRight(),
-		 * m_operatorController.povLeft()));
-		 * m_operatorController.povDown().onTrue(new HoodCommands.RunAtPower(-.1, 0));
-		 * m_operatorController.povUp().onTrue(new HoodCommands.RunAtPower(.1, 0));
-		 */
+		Drive.getDrive().setDefaultCommand(
+				new DriveCommands.JoystickDrive(
+						() -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(),
+						() -> m_driverController.getL2Axis() - m_driverController.getR2Axis(),
+						m_driverController.getHID()::getCreateButton));
+		Turret.getTurret().setDefaultCommand(
+				new TurretCommands.RunToAngleHardwareSignal(m_operatorController::getLeftX,
+						m_operatorController::getLeftY));
+		m_operatorController.L1().whileTrue(new TurretCommands.RunAtPower(-.1, 0));
+		m_operatorController.R1().whileTrue(new TurretCommands.RunAtPower(.1, 0));
+		m_operatorController.triangle().toggleOnTrue(
+				new ShooterCommands.RunAtDPadRPM(this, m_operatorController.povRight(),
+						m_operatorController.povLeft()));
+		m_operatorController.povDown().onTrue(new HoodCommands.RunAtPower(-.1, 0));
+		m_operatorController.povUp().onTrue(new HoodCommands.RunAtPower(.1, 0));
 		m_operatorController.L2().whileTrue(new IntakeCommands.SpinArmPower(0.1));
 		m_operatorController.R2().whileTrue(new IntakeCommands.SpinArmPower(-0.1));
 		m_operatorController.square().onTrue(new IntakeCommands.SpinIntake(.1));
