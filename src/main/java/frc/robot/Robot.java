7b294232d883ab2adda41990eea93a39245a4865
Wesley D. Turner <wdturner@gmail.com>
2026-03-09 19:12:01 -0400

    Blank out robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,30 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RepeatCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
-import frc.robot.Constants.Subsystems.AgitatorConstants;
-import frc.robot.Constants.Subsystems.IntakeConstants;
-import frc.robot.commands.AimCommands;
-import frc.robot.commands.AimCommands.AdjustAim;
-import frc.robot.commands.AngularPositionCommands;
-import frc.robot.commands.ClimberCommands;
-import frc.robot.commands.DriveCommands;
-import frc.robot.commands.IntakeCommands;
-import frc.robot.commands.ShooterCommands;
-import frc.robot.commands.TransportCommands;
-import frc.robot.subsystems.Agitator;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Hood;
-import frc.robot.subsystems.IntakeArm;
-import frc.robot.subsystems.IntakeWheels;
-import frc.robot.subsystems.Kicker;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Turret;
-import frc.robot.subsystems.Vision;
 
 public class Robot extends TimedRobot {
 	private CommandScheduler m_scheduler = CommandScheduler.getInstance();
@@ -43,30 +20,34 @@ public class Robot extends TimedRobot {
 	private final CommandPS5Controller m_operatorController = new CommandPS5Controller(
 			Constants.ControllerConstants.kOperatorControllerPort);
 	private final Aim m_aim = new Aim.Linear();
-	private final Command m_auto;
-	private final Command m_auto2;
+	private final Command m_auto = null;
+	private final Command m_auto2 = null;
 
 	{ // Here are the individual subsystems
-		new Drive();
-		new Shooter();
-		new IntakeWheels();
-		new IntakeArm();
-		new Climber();
-		new Kicker();
-		new Agitator();
-		new Vision();
-		Turret.create();
-		Hood.create();
+		/*
+		 * new Drive();
+		 * new Shooter();
+		 * new IntakeWheels();
+		 * new IntakeArm();
+		 * new Climber();
+		 * new Kicker();
+		 * new Agitator();
+		 * new Vision();
+		 * Turret.create();
+		 * Hood.create();
+		 */
 	}
 
 	public void initSubsystems() {
-		Hood.getHood().stop();
-		Climber.getClimber().stopMotor();
-		IntakeArm.getIntakeArm().stopMotor();
-		IntakeWheels.stopWheel();
-		Agitator.stop();
-		Kicker.stop();
-		Shooter.stop();
+		/*
+		 * Hood.getHood().stop();
+		 * Climber.getClimber().stopMotor();
+		 * IntakeArm.getIntakeArm().stopMotor();
+		 * IntakeWheels.stopWheel();
+		 * Agitator.stop();
+		 * Kicker.stop();
+		 * Shooter.stop();
+		 */
 	}
 
 	{ // Here is the auto currently being run
@@ -78,14 +59,16 @@ public class Robot extends TimedRobot {
 		// new DriveCommands.DriveDistance(-1.5),
 		// new DriveCommands.DriveDistance(1.5));
 
-		m_auto = new SequentialCommandGroup(
-				new AdjustAim(true, 12.2, this).withTimeout(2),
-				new AngularPositionCommands.RunToAngleHardware(Turret.getTurret(), 36,
-						Turret.getConstants()).withTimeout(1),
-				new WaitCommand(3),
-				TransportCommands.getTimedShoot(10));
-
-		m_auto2 = new AngularPositionCommands.RunToAngleHardware(Turret.getTurret(), 36, Turret.getConstants());
+		/*
+		 * m_auto = new SequentialCommandGroup(
+		 * new AdjustAim(true, 12.2, this).withTimeout(2),
+		 * new AngularPositionCommands.RunToAngleHardware(Turret.getTurret(), 36,
+		 * Turret.getConstants()).withTimeout(1),
+		 * new WaitCommand(3),
+		 * TransportCommands.getTimedShoot(10));
+		 * m_auto2 = new AngularPositionCommands.RunToAngleHardware(Turret.getTurret(),
+		 * 36, Turret.getConstants());
+		 */
 
 		// Commands.sequence(
 		// new AngularPositionCommands.RunToAngleSoftware(Turret.getTurret(), 0,
@@ -104,24 +87,30 @@ public class Robot extends TimedRobot {
 
 		// *************** DRIVER BINDINGS ***************
 		{ // Drive bindings
-			Drive.getDrive().setDefaultCommand(
-					new DriveCommands.JoystickDrive(
-							() -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(),
-							() -> m_driverController.getL2Axis() - m_driverController.getR2Axis(), // L2 rotates left,
-																									// R2 rotates right
-							m_driverController.getHID()::getCreateButton));
-			m_driverController.options().debounce(0.1).onTrue(new DriveCommands.ResetHeading());
+			/*
+			 * Drive.getDrive().setDefaultCommand(
+			 * new DriveCommands.JoystickDrive(
+			 * () -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(),
+			 * () -> m_driverController.getL2Axis() - m_driverController.getR2Axis(), // L2
+			 * rotates left,
+			 * // R2 rotates right
+			 * m_driverController.getHID()::getCreateButton));
+			 * m_driverController.options().debounce(0.1).onTrue(new
+			 * DriveCommands.ResetHeading());
+			 */
 		}
 
 		{ // Hood bindings
-			m_driverController.cross().whileTrue(
-					new AngularPositionCommands.RunAtPower(Hood.getHood(),
-							-.2, /* POWER */
-							0).withInterruptBehavior(Command.InterruptionBehavior.kCancelIncoming)); /* TIME */
-			m_driverController.triangle().whileTrue(
-					new AngularPositionCommands.RunAtPower(Hood.getHood(),
-							.2, /* POWER */
-							0)); /* TIME */
+			/*
+			 * m_driverController.cross().whileTrue(
+			 * new AngularPositionCommands.RunAtPower(Hood.getHood(),
+			 * -.2,
+			 * 0).withInterruptBehavior(Command.InterruptionBehavior.kCancelIncoming));
+			 * m_driverController.triangle().whileTrue(
+			 * new AngularPositionCommands.RunAtPower(Hood.getHood(),
+			 * .2,
+			 * 0));
+			 */
 			// m_driverController.cross()
 			// .whileTrue(new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 0,
 			// Hood.getConstants()));
@@ -130,61 +119,88 @@ public class Robot extends TimedRobot {
 		{ // Intake bindings
 			// TODO uncomment when robot fixed
 			// //m_operatorController.options().debounce(0.1).onTrue(IntakeCommands.getEncoderResetCommand());
-			m_driverController.R1().debounce(0.1).onTrue(IntakeCommands.getOutCommand());// Deploys arm TODO: tune
-																							// position
-			m_driverController.L1().debounce(0.1).onTrue(
-					Commands.sequence(
-							new IntakeCommands.StopIntake(),
-							new TransportCommands.StopAgitator(),
-							IntakeCommands.getInCommand()));// Retracts arm and stops power TODO: tune position
-			m_driverController.povRight().whileTrue(IntakeCommands.getRunArmAtPowerCommand(-0.2));
-			m_driverController.povLeft().whileTrue(IntakeCommands.getRunArmAtPowerCommand(0.2));
+			/*
+			 * m_driverController.R1().debounce(0.1).onTrue(IntakeCommands.getOutCommand());
+			 * // Deploys arm TODO: tune
+			 * // position
+			 * m_driverController.L1().debounce(0.1).onTrue(
+			 * Commands.sequence(
+			 * new IntakeCommands.StopIntake(),
+			 * new TransportCommands.StopAgitator(),
+			 * IntakeCommands.getInCommand()));// Retracts arm and stops power TODO: tune
+			 * position
+			 * m_driverController.povRight().whileTrue(IntakeCommands.
+			 * getRunArmAtPowerCommand(-0.2));
+			 * m_driverController.povLeft().whileTrue(IntakeCommands.getRunArmAtPowerCommand
+			 * (0.2));
+			 */
 		}
 
 		{ // Climber bindings
-			m_driverController.povUp().whileTrue(ClimberCommands.getRunAtPowerCommand(.6));
-			m_driverController.povDown().whileTrue(ClimberCommands.getRunAtPowerCommand(-0.6));
+			/*
+			 * m_driverController.povUp().whileTrue(ClimberCommands.getRunAtPowerCommand(.6)
+			 * );
+			 * m_driverController.povDown().whileTrue(ClimberCommands.getRunAtPowerCommand(-
+			 * 0.6));
+			 */
 		}
 
 		// *************** OPERATOR BINDINGS ***************
 
 		{ // Turret bindings
-			m_operatorController.L2().whileTrue(
-					new AngularPositionCommands.RunAtPower(Turret.getTurret(), -.2, 0));// Rotates
-			// left/counterclockwise
-			m_operatorController.R2().whileTrue(new AngularPositionCommands.RunAtPower(Turret.getTurret(), .2, 0));// Rotates
+			/*
+			 * m_operatorController.L2().whileTrue(
+			 * new AngularPositionCommands.RunAtPower(Turret.getTurret(), -.2, 0));//
+			 * Rotates
+			 * // left/counterclockwise
+			 * m_operatorController.R2().whileTrue(new
+			 * AngularPositionCommands.RunAtPower(Turret.getTurret(), .2, 0));// Rotates
+			 */
 			// right/clockwise
 		}
 
 		{ // Transport commands
-			m_operatorController.R1().whileTrue( // Runs agitator and kicker (for shooting) when pressed on R1
-					Commands.parallel(
-							new TransportCommands.RunAgitatorAtPower(AgitatorConstants.kTeleopPower)));
-			// new TransportCommands.RunKickerAtPower(KickerConstants.kTeleopPower)));
-			m_operatorController.povLeft()
-					.whileTrue(new TransportCommands.RunAgitatorAtPower(-AgitatorConstants.kTeleopPower));
-			m_operatorController.L1().debounce(.05)
-					.toggleOnTrue(new IntakeCommands.Spintake(IntakeConstants.kWheelPower));
+			/*
+			 * m_operatorController.R1().whileTrue( // Runs agitator and kicker (for
+			 * shooting) when pressed on R1
+			 * Commands.parallel(
+			 * new TransportCommands.RunAgitatorAtPower(AgitatorConstants.kTeleopPower)));
+			 * // new TransportCommands.RunKickerAtPower(KickerConstants.kTeleopPower)));
+			 * m_operatorController.povLeft()
+			 * .whileTrue(new
+			 * TransportCommands.RunAgitatorAtPower(-AgitatorConstants.kTeleopPower));
+			 * m_operatorController.L1().debounce(.05)
+			 * .toggleOnTrue(new IntakeCommands.Spintake(IntakeConstants.kWheelPower));
+			 */
 			// m_operatorController.create().whileTrue(new
 			// IntakeCommands.Spintake(-IntakeConstants.kWheelPower));
 		}
 
 		{ // Shooting bindings
-			m_operatorController.square().onTrue(new ShooterCommands.Stop());
-			boolean absolute = true;
-			m_operatorController.triangle().debounce(.1).onTrue(new AimCommands.AdjustAim(absolute, 4.5, this));
-			m_operatorController.circle().debounce(.1).onTrue(new AimCommands.AdjustAim(absolute, 9.1, this));
-			m_operatorController.cross().debounce(.1).onTrue(new AimCommands.AdjustAim(absolute, 13, this));
-
-			m_operatorController.create().whileTrue(
-					new RepeatCommand(new AimCommands.AutoAim(Turret.getTurret(), Vision.getVision())));
+			/*
+			 * m_operatorController.square().onTrue(new ShooterCommands.Stop());
+			 * boolean absolute = true;
+			 * m_operatorController.triangle().debounce(.1).onTrue(new
+			 * AimCommands.AdjustAim(absolute, 4.5, this));
+			 * m_operatorController.circle().debounce(.1).onTrue(new
+			 * AimCommands.AdjustAim(absolute, 9.1, this));
+			 * m_operatorController.cross().debounce(.1).onTrue(new
+			 * AimCommands.AdjustAim(absolute, 13, this));
+			 * m_operatorController.create().whileTrue(
+			 * new RepeatCommand(new AimCommands.AutoAim(Turret.getTurret(),
+			 * Vision.getVision())));
+			 */
 			// m_operatorController.triangle().debounce(0.1).onTrue(
 			// new ShooterCommands.RunAtDPadRPM(this, m_operatorController.povRight(),
 			// m_operatorController.povLeft()));
 
-			absolute = false;
-			m_operatorController.povUp().whileTrue(new AimCommands.AdjustAim(absolute, 5, this)); // 5 ft/s increasing
-			m_operatorController.povDown().whileTrue(new AimCommands.AdjustAim(absolute, -5, this));
+			/*
+			 * absolute = false;
+			 * m_operatorController.povUp().whileTrue(new AimCommands.AdjustAim(absolute, 5,
+			 * this)); // 5 ft/s increasing
+			 * m_operatorController.povDown().whileTrue(new AimCommands.AdjustAim(absolute,
+			 * -5, this));
+			 */
 			/*
 			 * m_operatorController.square().toggleOnTrue(
 			 * new AimCommands.RiyaAiming(m_operatorController.povUp(),
@@ -197,68 +213,80 @@ public class Robot extends TimedRobot {
 	}
 
 	private void bindTestControls() {
-		Drive.getDrive().setDefaultCommand(
-				new DriveCommands.JoystickDrive(
-						() -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(),
-						() -> m_driverController.getR2Axis() - m_driverController.getL2Axis(),
-						m_driverController.getHID()::getCreateButton));
-
-		m_driverController.cross().whileTrue(
-				new TransportCommands.RunAgitatorAtPower(
-						0.75 /* POWER */));
-		m_driverController.square().whileTrue(
-				new TransportCommands.RunKickerAtPower(
-						0.4 /* POWER */));
-		m_operatorController.L2().whileTrue(
-				new TransportCommands.RunAgitatorAtPower(
-						-0.75)); // POWER
+		/*
+		 * Drive.getDrive().setDefaultCommand(
+		 * new DriveCommands.JoystickDrive(
+		 * () -> -m_driverController.getLeftY(), () -> -m_driverController.getLeftX(),
+		 * () -> m_driverController.getR2Axis() - m_driverController.getL2Axis(),
+		 * m_driverController.getHID()::getCreateButton));
+		 * m_driverController.cross().whileTrue(
+		 * new TransportCommands.RunAgitatorAtPower(
+		 * 0.75 ));
+		 * m_driverController.square().whileTrue(
+		 * new TransportCommands.RunKickerAtPower(
+		 * 0.4 ));
+		 * m_operatorController.L2().whileTrue(
+		 * new TransportCommands.RunAgitatorAtPower(
+		 * -0.75)); // POWER
+		 */
 
 		/*
 		 * Turret.getTurret().setDefaultCommand(
 		 * new TurretCommands.RunToAngleHardwareSignal(m_operatorController::getLeftX,
 		 * m_operatorController::getLeftY));
 		 */
-		m_operatorController.L1().whileTrue(
-				new AngularPositionCommands.RunAtPower(Turret.getTurret(),
-						.3, /* POWER */
-						0)); /* TIME */
-		m_operatorController.R1().whileTrue(
-				new AngularPositionCommands.RunAtPower(Turret.getTurret(),
-						-.3, /* POWER */
-						0)); /* TIME */
-
-		m_operatorController.triangle().debounce(0.1).onTrue(
-				new ShooterCommands.RunAtDPadRPM(this, m_operatorController.povRight(),
-						m_operatorController.povLeft()));
+		/*
+		 * m_operatorController.L1().whileTrue(
+		 * new AngularPositionCommands.RunAtPower(Turret.getTurret(),
+		 * .3,
+		 * 0));
+		 * m_operatorController.R1().whileTrue(
+		 * new AngularPositionCommands.RunAtPower(Turret.getTurret(),
+		 * -.3,
+		 * 0));
+		 * m_operatorController.triangle().debounce(0.1).onTrue(
+		 * new ShooterCommands.RunAtDPadRPM(this, m_operatorController.povRight(),
+		 * m_operatorController.povLeft()));
+		 */
 
 		/*
 		 * m_operatorController.triangle().debounce(0.1).toggleOnTrue(
 		 * new ShooterCommands.RunAtDynamicRPM(1000));
 		 */
 
-		m_operatorController.povDown().whileTrue(
-				new AngularPositionCommands.RunAtPower(Hood.getHood(),
-						-.2, /* POWER */
-						0)); /* TIME */
-		m_operatorController.povUp().whileTrue(
-				new AngularPositionCommands.RunAtPower(Hood.getHood(),
-						.2, /* POWER */
-						0)); /* TIME */
-
-		m_operatorController.touchpad()
-				.onTrue(new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 0, Hood.getConstants()));
+		/*
+		 * m_operatorController.povDown().whileTrue(
+		 * new AngularPositionCommands.RunAtPower(Hood.getHood(),
+		 * -.2,
+		 * 0));
+		 * m_operatorController.povUp().whileTrue(
+		 * new AngularPositionCommands.RunAtPower(Hood.getHood(),
+		 * .2,
+		 * 0));
+		 * m_operatorController.touchpad()
+		 * .onTrue(new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 0,
+		 * Hood.getConstants()));
+		 */
 
 		{ // climber test bindings
-			m_driverController.triangle().whileTrue(ClimberCommands.getRunAtPowerCommand(.6));
-			m_driverController.circle().whileTrue(ClimberCommands.getRunAtPowerCommand(-0.6));
+			/*
+			 * m_driverController.triangle().whileTrue(ClimberCommands.getRunAtPowerCommand(
+			 * .6));
+			 * m_driverController.circle().whileTrue(ClimberCommands.getRunAtPowerCommand(-0
+			 * .6));
+			 */
 		}
 
 		{ // intake test bindings
-			m_operatorController.circle().whileTrue(IntakeCommands.getRunArmAtPowerCommand(-0.2));
-			m_operatorController.cross().whileTrue(IntakeCommands.getRunArmAtPowerCommand(0.2));
-			m_operatorController.square().debounce(0.1).toggleOnTrue(
-					new IntakeCommands.Spintake(
-							IntakeConstants.kWheelPower)); /* POWER */
+			/*
+			 * m_operatorController.circle().whileTrue(IntakeCommands.
+			 * getRunArmAtPowerCommand(-0.2));
+			 * m_operatorController.cross().whileTrue(IntakeCommands.getRunArmAtPowerCommand
+			 * (0.2));
+			 * m_operatorController.square().debounce(0.1).toggleOnTrue(
+			 * new IntakeCommands.Spintake(
+			 * IntakeConstants.kWheelPower));
+			 */
 		}
 
 	}
