f918f036b99818ec42c3b39a9a827a696dab5e34
nitya2-0 <gkrishan75@gmail.com>
2026-03-13 21:42:09 -0400

    Adding back untested changes

--- src/main/java/frc/robot/autos/AutoComposer.java
+++ src/main/java/frc/robot/autos/AutoComposer.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.Subsystems.AutoConstants;
 import frc.robot.commands.AimCommands.AdjustAim;
 import frc.robot.commands.AngularPositionCommands;
 import frc.robot.commands.DriveCommands;
@@ -30,6 +31,7 @@ public class AutoComposer {
 	}
 
 	public Command getLeftOneShootAuto() {
+		AutoConstants.isBackwardsAtStart = true; // Untested
 		return new SequentialCommandGroup(
 				getShootCommand(92 + (92 - 36), 11.6, 6),
 				new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 100,
@@ -37,6 +39,7 @@ public class AutoComposer {
 	}
 
 	public Command getRightOneShootAuto() {
+		AutoConstants.isBackwardsAtStart = true; // Untested
 		return new SequentialCommandGroup(
 				getShootCommand(36, 11.6, 6),
 				new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 100,
@@ -44,6 +47,7 @@ public class AutoComposer {
 	}
 
 	public Command getRightTwoShootAuto() {
+		AutoConstants.isBackwardsAtStart = true; // Untested
 		return new SequentialCommandGroup(
 				getShootCommand(36, 11.6, 6),
 				new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 100,
@@ -60,6 +64,7 @@ public class AutoComposer {
 	}
 
 	public Command getRightTwoShootAutoPracticeModified() {
+		AutoConstants.isBackwardsAtStart = true; // Untested
 		return new SequentialCommandGroup(
 				getShootCommand(36, 9.1, 6),
 				new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 100,
@@ -75,6 +80,7 @@ public class AutoComposer {
 	}
 
 	public Command getRightTwoShootAutoWithIntake() {
+		AutoConstants.isBackwardsAtStart = true; // Untested
 		return new SequentialCommandGroup(
 				getShootCommand(36, 11.6, 6),
 				new AngularPositionCommands.RunToAngleHardware(Hood.getHood(), 100,
--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -36,6 +36,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
+import frc.robot.Constants.Subsystems.AutoConstants;
 import frc.robot.SwerveModule;
 
 public class Drive extends SubsystemBase {
@@ -114,7 +115,8 @@ public class Drive extends SubsystemBase {
 	 * @return The heading
 	 */
 	public static Rotation2d getHeading() {
-		return s_theDrive.m_gyro.getRotation2d();
+		return s_theDrive.m_gyro.getRotation2d() // Untested
+				.rotateBy((AutoConstants.isBackwardsAtStart) ? Rotation2d.k180deg : Rotation2d.kZero);
 	}
 
 	/**
