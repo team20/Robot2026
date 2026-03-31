055a48dff1a8545ab98a5412f79642126317d064
Vincent-Christianson <vindezlch@gmail.com>
2026-03-30 21:43:09 -0400

    Still learning
    I will try my best to get this working but don't expect and quick progress.

--- src/main/java/frc/robot/subsystems/RainbowParty.java
+++ src/main/java/frc/robot/subsystems/RainbowParty.java
@@ -1,17 +1,17 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RainbowParty extends SubsystemBase {
-/** The USB port that's used to connect to the Arduino. */
+	/** The USB port that's used to connect to the Arduino. */
 	private SerialPort m_usb;
 
-		private SerialPort m_usb;
-
-	/** The bytes that control the LED mode */
-	public enum StatusCode {
+	/* The bytes that control the LED mode */
+	private enum StatusCode {
 		RESET((byte) 0),
 		RAINBOW_PARTY_FUN_TIME((byte) 1),
 		SOLID_ORANGE((byte) 2),
@@ -28,27 +28,35 @@ public class RainbowParty extends SubsystemBase {
 		}
 	}
 
-		
 	/** Creates a new ArduinoSubsystem. */
-	public ArduinoSubsystem() {
+	public RainbowParty() {
 		try {
 			m_usb = new SerialPort(9600, Port.kUSB);
+			Thread.sleep(2000); // allow Arduino to reboot
 		} catch (Exception e) {
 			DriverStation.reportError("Could not initialize Arduino over USB", false);
 			m_usb = null;
 		}
-		setCode(StatusCode.DEFAULT);
+		setCode(StatusCode.RESET);
 	}
 
+	public void startCode() {
+		setCode(StatusCode.RESET);
+	}
 
+	public Command Reset() {
+		return runOnce(() -> setCode(StatusCode.RESET));
+	}
 
-blic void setCode(StatusCode code) {
+	public void setCode(StatusCode code) {
 		if (m_usb != null) {
 
 			m_usb.write(new byte[] { code.code }, 1);
 		}
 	}
 
+	public Command writeStatus(StatusCode code) {
+		return runOnce(() -> setCode(code));
+	}
 
-
-lic Command writeStatus(StatusCode 
+}
